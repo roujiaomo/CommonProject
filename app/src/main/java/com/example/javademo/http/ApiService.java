f4675ecd3fad9a8294cbb209a48b80fe4bc271fc
dhc-changhu <yx09020502>
2020-06-17 14:06:13 +0800

    test-1

--- app/src/main/java/com/example/javademo/http/ApiService.java
+++ app/src/main/java/com/example/javademo/http/ApiService.java
@@ -40,9 +40,6 @@ public interface ApiService {
     Observable<BaseResponse<TranslationBean>> getNews();
 
 //  appKey  3ee5b1f1af26cb1baab51209970eac46
-@GET(".php?a=fy&f=auto&t=auto&w=hello%20fuck")
-Observable<BaseResponse<TranslationBean>> getNewsss();
-    @GET("index")
     Observable<WeatherBean> getWeather(@Query("cityname") String cityname, @Query("key") String key);
 
     /**
